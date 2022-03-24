fcf52487452b1a6773fde525f80cd81a093b42e1
victorazangu <victorsazangu@gmail.com>
2022-03-24 20:15:06 +0300

    number 8

--- 0x06-pointers_arrays_strings/100-main.c
+++ 0x06-pointers_arrays_strings/100-main.c
@@ -9,20 +9,20 @@
 int main(void)
 {
   char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
-  char *ptr;
+  char *p;
 
   p = rot13(s);
-  printf("%s", ptr);
+  printf("%s", p);
   printf("------------------------------------\n");
   printf("%s", s);
   printf("------------------------------------\n");
   p = rot13(s);
-  printf("%s", ptr);
+  printf("%s", p);
   printf("------------------------------------\n");
   printf("%s", s);
   printf("------------------------------------\n");
   p = rot13(s);
-  printf("%s", ptr);
+  printf("%s", p);
   printf("------------------------------------\n");
   printf("%s", s);
   return (0);
